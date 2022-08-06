1cde4ff2fb7af791dd6d1dfc38add9fd7d14ab59
Tuan Kiet <thongtuankiet.1999@gmail.com>
2022-08-06 17:45:13 +0700

    first commit

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { Text, View } from 'react-native'
+
+const HomeScreen: React.FC = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
---
+++ types.tsx
@@ -0,0 +1,16 @@
+export type RootStackParamList = {
+  Root: undefined;
+};
+
+export type BottomTabParamList = {
+  TabOne: undefined;
+  TabTwo: undefined;
+};
+
+export type TabOneParamList = {
+  TabOneScreen: undefined;
+};
+
+export type TabTwoParamList = {
+  TabTwoScreen: undefined;
+};
